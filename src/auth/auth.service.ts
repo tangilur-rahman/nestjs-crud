9cddd242a349f5d4d0110c2ba811fa817475710b
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2025-10-25 18:30:21 +0600

    register user done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import bcrypt from 'bcrypt';
 import { RegisterUserDTO } from 'src/dto/register-user.dto';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async registerUser(registerUserDto: RegisterUserDTO) {
     // hashing password before creating user
     const hash = await bcrypt.hash(registerUserDto.password, 10);
 
-    return await this.userService.createUser({
+    // create user
+    const createdUser = (await this.userService.createUser({
       ...registerUserDto,
       password: hash,
-    });
+    })) as unknown as { id: number; email: string };
+
+    // generate jwt token for the user
+    const payload = { sub: createdUser.id, email: createdUser.email };
+    const token = await this.jwtService.signAsync(payload);
+
+    return { createdUser, token };
   }
 }
--- src/dto/register-user.dto.ts
+++ src/dto/register-user.dto.ts
@@ -1,6 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 export class RegisterUserDTO {
+  @IsNotEmpty()
+  @IsString({ message: 'First name must be a string' })
   fName: string;
+
+  @IsNotEmpty()
+  @IsString({ message: 'Last name must be a string' })
   lName: string;
+
+  @IsEmail({}, { message: 'Email must be a valid email address' })
   email: string;
+
+  @IsNotEmpty()
+  @IsString({ message: 'Password must be a string' })
   password: string;
 }
