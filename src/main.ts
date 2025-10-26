aebea8fdcf5671f100767569625c010abb06e832
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2025-10-26 15:54:21 +0600

    documentation added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // for validation input data
   app.useGlobalPipes(new ValidationPipe());
+
+  // for documentation
+  const config = new DocumentBuilder()
+    .setTitle('NextJs CRUD example')
+    .setDescription('The NextJs CRUD API description')
+    .setVersion('1.0')
+    .addTag('NextJs CRUD')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  // start server
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
